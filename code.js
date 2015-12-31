6f8ede7deb7b92f3e51614a4fae7d69ad392a99a
Zac Hansen <xaxxon@gmail.com>
2015-12-31 02:35:09 -0800

    javascript test code to test the features in v8classwrapper

---
+++ code.js
@@ -0,0 +1,24 @@
+var g = x=>x+2;
+
+g(5);
+
+var step;
+var p;
+var p2;
+// for (step = 0; step < 5000000; step++) {
+// 	p = new Point(2,2);
+// 	p2= new Point();
+// 	p2.x = 2;
+// 	p.x + p2.x
+// }
+var p3 = new Point();
+var result = p3.thing(55,"hello");
+result;
+
+p3.stringthing();
+
+p3.make_point().thing(1,"asdf");
+
+
+p3.x=13;
+p3.x;
