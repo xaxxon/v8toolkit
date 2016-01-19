cbface5b5478835abaa2e68920217243fae8f507
Zac Hansen <xaxxon@gmail.com>
2016-01-19 01:47:10 -0800

    made some sanity tests in javascript and added print helpers to be able to print from javascript to stdout

--- code.js
+++ code.js
@@ -3,9 +3,26 @@ var line = new Line();
 println("line.get_point().get_foo().i = %d", line.get_point().get_foo().i);
 
 
-// getting two objects by rvalue should have different underlying c++ objects
-
-
+println("This object is not a c++ wrapped object");
+printobj({});
 
 // getting two objects by reference should have the same underlying c++ objects
+//   because they return an object by reference
+println("These two objects should have the same backing c++ object");
+var p1 = line.get_point();
+var p2 = line.get_point();
+
+printobj(p1);
+printobj(p2);
+println("(known limitation) These objects should be the same javascript object as well: %s", p1===p2?"same": "different");
+
+// getting two objects by rvalue should have different underlying c++ objects
+//   because they return on object as an rvalue that must have a copy made
+println("These two objects should have different backing c++ object");
+var l1 = line.get_point().get_foo();
+var l2 = line.get_point().get_foo();
+printobj(l1);
+printobj(l2);
+println("These objects should not be the same javascript object, either: %s", l1===l2?"same": "different");
+
 
--- v8_class_wrapper.hpp
+++ v8_class_wrapper.hpp
@@ -43,7 +43,7 @@ struct DestructorBehaviorLeaveAlone : DestructorBehavior<T> {
 };
 
 
-// Specialized types that know how to convert from a v8::Value to a primitive type
+// Specialized types that know how to convert from a v8::Value to a specific primitive type
 template<typename T>
 struct CastToNative {};
 
@@ -250,7 +250,6 @@ public:
 	*   even though V8 will still present the ability to your javascript (I think)
 	*/
 	virtual ~V8ClassWrapper(){
-
 		// this was happening when it wasn't supposed to, like when making temp copies.   need to disable copying or something
 		//   if this line is to be added back
 		// isolate_to_wrapper_map.erase(this->isolate);
@@ -495,7 +494,6 @@ void V8ClassWrapper<T>::GetterHelper(v8::Local<v8::String> property,
 
 #include <boost/format.hpp>
 static void print_helper(const v8::FunctionCallbackInfo<v8::Value>& args, bool append_newline) {
-
 	if (args.Length() > 0) {
 		auto string = *v8::String::Utf8Value(args[0]);
 		auto format = boost::format(string);
@@ -503,6 +501,7 @@ static void print_helper(const v8::FunctionCallbackInfo<v8::Value>& args, bool a
 		for (int i = 1; i < args.Length(); i++) {
 			format % *v8::String::Utf8Value(args[i]);
 		}
+		std::cout << ">>> ";
 		std::cout << format;	
 		if (append_newline) {
 			std::cout << std::endl;
@@ -512,16 +511,32 @@ static void print_helper(const v8::FunctionCallbackInfo<v8::Value>& args, bool a
 
 
 
-static void print_callback(const v8::FunctionCallbackInfo<v8::Value>& args) {
+
+// Helpers for print debugging, not actually core to the library
+void print_callback(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	return print_helper(args, false);
 }
 
-static void println_callback(const v8::FunctionCallbackInfo<v8::Value>& args) {
+void println_callback(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	return print_helper(args, true);
 }
 
+void printobj_callback(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	for (int i = 0; i < args.Length(); i++) {
+		auto object = v8::Object::Cast(*args[i]);
+		if(object->InternalFieldCount() > 0) {
+			v8::Local<v8::External> wrap = v8::Local<v8::External>::Cast(object->GetInternalField(0));
+			printf(">>> Object %p: %s\n", wrap->Value(), *v8::String::Utf8Value(args[i]));
+		} else {
+			printf(">>> Object does not appear to be a wrapped c++ class (no internal fields): %s\n", *v8::String::Utf8Value(args[i]));
+		}
+	}
+	// v8::Local<v8::Object> self = info.Holder();
+	// v8::Local<v8::External> wrap = v8::Local<v8::External>::Cast(self->GetInternalField(0));
+	// T * cpp_object = static_cast<T *>(wrap->Value());
+}
 
-
+// call this to add a function called "print" to whatever object template you pass in (probably the global one)
 void add_print(v8::Isolate * isolate, v8::Local<v8::ObjectTemplate> global_template )
 {
 	v8::HandleScope hs(isolate);
@@ -530,6 +545,9 @@ void add_print(v8::Isolate * isolate, v8::Local<v8::ObjectTemplate> global_templ
 	
 	auto println_template = v8::FunctionTemplate::New(isolate, &println_callback);
 	global_template->Set(isolate, "println", println_template);
+	
+	auto printobj_template = v8::FunctionTemplate::New(isolate, &printobj_callback);
+	global_template->Set(isolate, "printobj", printobj_template);
 	
 }
 
