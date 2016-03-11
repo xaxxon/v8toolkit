0d326b7e429a4caddf717cd429d8605c9768d423
Zac Hansen <xaxxon@gmail.com>
2016-03-10 23:29:09 -0800

    updated gh-pages docs

--- docs/html/search/classes_4.js
+++ docs/html/search/classes_4.js
@@ -3,5 +3,7 @@ var searchData=
   ['factory',['Factory',['../classv8toolkit_1_1Factory.html',1,'v8toolkit']]],
   ['factory_3c_20base_2c_20constructorargs_2e_2e_2e_20_3e',['Factory&lt; Base, ConstructorArgs... &gt;',['../classv8toolkit_1_1Factory.html',1,'v8toolkit']]],
   ['factory_3c_20realclass_2c_20constructorparameters_2e_2e_2e_20_3e',['Factory&lt; RealClass, ConstructorParameters... &gt;',['../classv8toolkit_1_1Factory.html',1,'v8toolkit']]],
-  ['foo',['Foo',['../structFoo.html',1,'']]]
+  ['foo',['Foo',['../structFoo.html',1,'']]],
+  ['function_3c_20ret_28const_20v8_3a_3afunctioncallbackinfo_3c_20v8_3a_3avalue_20_3e_20_26_2c_20tail_2e_2e_2e_29_3e',['function&lt; RET(const v8::FunctionCallbackInfo&lt; v8::Value &gt; &amp;, TAIL...)&gt;',['../structv8toolkit_1_1ParameterBuilder_3_01depth_00_01FUNCTION__TYPE_00_01std_1_1function_3_01RET_0f4aba7d23e057993acc750786dbf87cc.html',1,'v8toolkit']]],
+  ['function_3c_20void_28const_20v8_3a_3afunctioncallbackinfo_3c_20v8_3a_3avalue_20_3e_20_26_29_3e',['function&lt; void(const v8::FunctionCallbackInfo&lt; v8::Value &gt; &amp;)&gt;',['../structv8toolkit_1_1ParameterBuilder_3_01depth_00_01T_00_01std_1_1function_3_01void_07const_01v8_220d0f805e9cbbe0c7ae5ab5c333d395.html',1,'v8toolkit']]]
 ];
