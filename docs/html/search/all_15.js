6c1a42f94c08e78ed6b75aaa1b9c760fb67ffebb
Zac Hansen <xaxxon@gmail.com>
2016-06-23 02:40:34 -0700

    updated docs

--- docs/html/search/all_15.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['x',['x',['../javascript__sample_8cpp.html#a6150e0515f7202e2fb518f7206ed97dc',1,'javascript_sample.cpp']]],
-  ['x_5f',['x_',['../classPoint.html#acfe156c55546f7e551fb54c7ea08a6cb',1,'Point']]]
-];
