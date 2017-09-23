2d19bcd85b5d4130ddb28e4e59388e48025a5a0e
Zac Hansen <xaxxon@gmail.com>
2017-09-23 00:19:38 -0700

    loading class_parser data from sample.cpp

--- class_parser/test/test.cpp
+++ class_parser/test/test.cpp
@@ -1,9 +1,15 @@
 
+
+#include <fstream>
+#include <sstream>
+
 #include "../clang.h"
 #include "../ast_action.h"
 
 int main() {
+    std::ifstream sample_source("sample.cpp");
+    std::string sample_source_contents((std::istreambuf_iterator<char>(sample_source)),
+                    std::istreambuf_iterator<char>());
 
-
-    clang::tooling::runToolOnCode(new v8toolkit::class_parser::PrintFunctionNamesAction, "class X {};");
+    clang::tooling::runToolOnCode(new v8toolkit::class_parser::PrintFunctionNamesAction, sample_source_contents);
 }
