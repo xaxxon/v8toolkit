110dc285f259ab91ca48af3b7a4d7b17396a178e
Zac Hansen <xaxxon@gmail.com>
2017-10-17 05:48:21 -0700

    more progress on js stub output

--- class_parser/output_modules/javascript_stub_output.cpp
+++ class_parser/output_modules/javascript_stub_output.cpp
@@ -16,10 +16,14 @@ std::unique_ptr<xl::templates::Provider_Interface> get_provider(WrappedClass con
     return xl::templates::make_provider(
         std::pair("comment", c->comment),
         std::pair("name", c->get_name_alias()),
-        std::pair("data_members", xl::templates::make_provider(c->get_members()))
-
+        std::pair("data_members", xl::templates::make_provider(c->get_members())),
+        std::pair("constructors", xl::templates::make_provider(c->get_constructors())),
+        std::pair("member_functions", xl::templates::make_provider(c->get_member_functions())),
+        std::pair("static_functions", xl::templates::make_provider(c->get_static_functions())),
+        std::pair("inheritance", fmt::format("{}", c->base_types.empty() ? "" : (*c->base_types.begin())->get_name_alias()))
     );
 }
+
 
 std::unique_ptr<xl::templates::Provider_Interface> get_provider(std::unique_ptr<DataMember> const & d) {
     return xl::templates::make_provider(
@@ -27,11 +31,10 @@ std::unique_ptr<xl::templates::Provider_Interface> get_provider(std::unique_ptr<
         std::pair("name", d->js_name),
         std::pair("type", d->type.get_jsdoc_type_name())
     );
-
 }
 
 
-std::unique_ptr<xl::templates::Provider_Interface> get_provider(ClassFunction const * f) {
+std::unique_ptr<xl::templates::Provider_Interface> get_provider(std::unique_ptr<MemberFunction> const & f) {
     return xl::templates::make_provider(
         std::pair("name", f->js_name),
         std::pair("static", f->is_static ? "static" : ""),
@@ -41,14 +44,27 @@ std::unique_ptr<xl::templates::Provider_Interface> get_provider(ClassFunction co
 }
 
 
+std::unique_ptr<xl::templates::Provider_Interface> get_provider(std::unique_ptr<StaticFunction> const & f) {
+    return xl::templates::make_provider(
+        std::pair("name", f->js_name),
+        std::pair("static", f->is_static ? "static" : ""),
+        std::pair("parameters", xl::templates::make_provider(f->parameters)),
+        std::pair("return", xl::templates::make_provider(f->return_type))
+    );
+}
+
+
+
 std::unique_ptr<xl::templates::Provider_Interface> get_provider(ClassFunction::ParameterInfo const & p) {
     return xl::templates::make_provider("BOGUS PARAMETERINFO PROVIDER");
 }
+
 
 std::unique_ptr<xl::templates::Provider_Interface> get_provider(ClassFunction::TypeInfo const & p) {
     return xl::templates::make_provider("BOGUS TYPEINFO PROVIDER");
 
 }
+
 
 void JavascriptStubOutputModule::process(std::vector<WrappedClass const *> const & wrapped_classes) {
     auto templates = xl::templates::load_templates("javascript_stub_templates");
