98c580ae41166138612a47b7169548038095c763
Zac Hansen <xaxxon@gmail.com>
2017-12-01 00:00:55 -0800

    get_interesting_properties handles undefined values without crashing

--- src/v8toolkit.cpp
+++ src/v8toolkit.cpp
@@ -841,7 +841,12 @@ std::vector<std::string> get_interesting_properties(v8::Local<v8::Context> conte
 
 
     // if the current object's prototype is null, then this is the Object object, and we don't want it's properties
-     while (!current_object.IsEmpty() && !current_object->IsNull() && !current_object->GetPrototype()->IsNull()) {
+     while (!current_object.IsEmpty() &&
+         !current_object->IsNull() &&
+         !current_object->IsUndefined() &&
+         !current_object->GetPrototype()->IsNull()) {
+
+         v8toolkit::print_v8_value_details(current_object);
 
          // for some reason, requesting all properties crashes on constructor function objects like `Object`
          auto maybe_own_property_names = current_object->GetOwnPropertyNames(context, v8::PropertyFilter::ONLY_CONFIGURABLE);
